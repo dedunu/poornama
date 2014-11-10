a7e015b66aa41fa85b0c1af0570a6e5644ef5edc
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-10 21:34:46 +0530

    Added AuthenticationTest class

---
+++ code/poornama/test/src/test/java/com/poornama/logic/session/AuthenticationTest.java
@@ -0,0 +1,24 @@
+package com.poornama.logic.session;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dedunu on 11/10/14.
+ */
+public class AuthenticationTest {
+
+    @Test
+    public void doSuccessfulAuthentication(){
+        Authentication authentication = new Authentication();
+        boolean isSuccessful = authentication.doAuthenticate("dedunumax","rootroot");
+        Assert.assertTrue(isSuccessful);
+    }
+    
+    @Test
+    public void doUnsuccessfulAuthentication(){
+        Authentication authentication = new Authentication();
+        boolean isSuccessful = authentication.doAuthenticate("dedunumax","notroot");
+        Assert.assertTrue(!isSuccessful);
+    }
+}
