312abb55e84b3bc041ce48be33bf5aa067f90d03
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-11 21:35:19 +0530

    Added AJAX log page

---
+++ webapp/src/main/java/com/poornama/web/controller/SystemController.java
@@ -0,0 +1,44 @@
+package com.poornama.web.controller;
+
+import com.poornama.api.logging.GlobalLogger;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by dedunu on 11/11/14.
+ */
+@Controller
+@RequestMapping("/system/")
+public class SystemController {
+
+    private static Logger log = GlobalLogger.getLogger();
+    private static String className = SystemController.class.getName();
+
+    @RequestMapping(value = "logs", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("pageTitle", "Poornama Transport Service - Log");
+        return "system/logs";
+    }
+
+    @RequestMapping(value = "logsAJAX", method = RequestMethod.GET)
+    public void searchAJAX(HttpServletResponse response) throws IOException {
+        File file = new File("/opt/poornama/log/poornama.log");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        while((line = bufferedReader.readLine()) != null){
+            response.getWriter().println(line);
+        }
+        bufferedReader.close();
+        fileReader.close();
+    }
+}
---
+++ webapp/src/main/webapp/WEB-INF/views/system/logs.jsp
@@ -0,0 +1,30 @@
+<%@ include file="../template/header.jsp" %>
+<style scoped>
+
+</style>
+
+<pre>
+</pre>
+
+<script>
+  $(document).ready(function(){
+    getLogs();
+  });
+
+  window.setInterval(function() {
+    getLogs();
+  }, 1000);
+
+  function getLogs(){
+    $.ajax({
+      url: '${pageContext.request.contextPath}/system/logsAJAX',
+      type: 'GET',
+      cache: false,
+      success: function(result) {
+        $('pre').html(result);
+      }
+    });
+    $("html, body").animate({ scrollTop: $(document).height()-$(window).height() });
+  }
+</script>
+<%@ include file="../template/footer.jsp" %>
